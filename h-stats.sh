93cbc4a0dc1432b3b52314f0483826a2144f62cf
RetroMike <michaeldlesk@gmail.com>
2024-10-27 13:10:49 -0500

    Update h-stats.sh

--- h-stats.sh
+++ h-stats.sh
@@ -2,16 +2,12 @@
 
 . $MINER_DIR/$CUSTOM_MINER/h-manifest.conf
 
-STATS_UPTIME=$(curl --connect-timeout 2 --max-time 30 --silent --noproxy '*' http://127.0.0.1:4000/uptime)
-
-uptime=`echo $STATS_UPTIME | jq -r '.Seconds'`
-#uptime=$STATS_UPTIME.Seconds
-hs=[1]
+uptime=$(awk '{print $1}' /proc/uptime)
+hs=[0.001]
 temp=[1]
 fan=[1]
 ver="1.0.0"
 bus_numbers=[null]
 
-khs=14
-stats=$(jq -n --argjson uptime "$uptime" --argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" --arg algo "RainbowMiner" --arg ver "$ver" --argjson bus_numbers "$bus_numbers" '{$hs, $temp, $fan, $uptime, $algo, $ver, $bus_numbers}')
-
+khs=0.001
+stats=$(jq -n --argjson uptime "$uptime" --argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" --arg algo "RMT Server" --arg ver "$ver" --argjson bus_numbers "$bus_numbers" '{$hs, $temp, $fan, $uptime, $algo, $ver, $bus_numbers}')
